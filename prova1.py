5a062df0e26ee3c9890636f6342a2ae566c347c7
DavideZFC <davidemaranzfc@gmail.com>
2023-10-10 15:22:36 +0200

    mesh defined

--- prova1.py
+++ prova1.py
@@ -1,3 +1,19 @@
-from functions.misc.get_summatory_index import get_summatory_index
+from functions.misc.merge_feature_maps import *
+from functions.orthogonal.bases import *
 
-print(get_summatory_index(4,2))
+import numpy as np
+
+
+x = np.linspace(-1.,1.,10)
+y = np.copy(x)
+
+X, Y = np.meshgrid(x, y)
+X = np.concatenate(X)
+Y = np.concatenate(Y)
+
+feature_map_x = sincos_features(X, d=3)
+feature_map_y = sincos_features(Y, d=3)
+
+global_feature = merge_feature_maps([feature_map_x, feature_map_y])
+
+print(global_feature)
