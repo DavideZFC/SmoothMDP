41a46d69584b756427a47cd1554d6f17fc8d085c
DavideZFC <davidemaranzfc@gmail.com>
2023-10-16 09:24:33 +0200

    feature maps created

--- classes/auxiliari/Linear_replay_buffer.py
+++ classes/auxiliari/Linear_replay_buffer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from functions.misc.merge_feature_maps import merge_feature_maps
+from functions.misc.build_mesh import build_mesh
 from functions.orthogonal.bases import *
 
 class Linear_replay_buffer:
@@ -52,5 +53,27 @@ class Linear_replay_buffer:
         self.action_grid = np.linspace(action_space.low[0], action_space.high[0], discretize)
 
         self.action_features = self.feature_map(self.action_grid, d=self.approx_degree)
+
+    def build_next_state_action_mesh(self, state):
+
+        state_repeated = np.repeat(state[np.newaxis, :], self.action_grid.shape[0], axis=0)
+
+        return np.append(state_repeated, self.action_grid.reshape(-1,1), axis=1)
+    
+
+    def build_next_state_action_feature_map(self, state=0):
+
+        if state == 0:
+            state = self.next_state_buffer[0,:]
+
+        aux_buffer = self.build_next_state_action_mesh(state=state)
+
+        feature_maps = []
+        for i in range(self.state_space_dim+self.action_space_dim):
+            feature_maps.append(self.feature_map(aux_buffer[:, i], d=self.approx_degree))
+
+        return merge_feature_maps(feature_maps)
+
+
         
 
--- prova.py
+++ prova.py
@@ -5,16 +5,20 @@ env = gym.make('Pendulum-v1')
 
 buffer = Linear_replay_buffer(basis='poly', approx_degree=3, state_space_dim=env.observation_space.shape[0], action_space_dim=env.action_space.shape[0], numel=10000)
 
-state = env.reset()
+state = env.reset()[0]
+print(state)
 done = False
 while not done:
     action = env.action_space.sample()
     next_state, reward, terminated, truncated, _ = env.step(action)
     done = terminated or truncated
 
-    buffer.memorize(state[0], action, next_state[0], reward)
+    buffer.memorize(state, action, next_state, reward)
     state = next_state
 
 buffer.linear_converter()
+print(buffer.full_feature_map.shape)
 buffer.build_action_mesh(env.action_space)
-print(buffer.action_features)
+# print(buffer.next_state_buffer[0,:])
+S = buffer.build_next_state_action_feature_map()
+print(S.shape)
