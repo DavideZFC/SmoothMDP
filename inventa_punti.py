0dfe20f990bbf35c265f4d773a840713b0ee29f0
DavideZFC <davidemaranzfc@gmail.com>
2023-12-14 14:10:47 +0100

    optimal design started

--- inventa_punti.py
+++ inventa_punti.py
@@ -2,14 +2,15 @@ import numpy as np
 from functions.misc.merge_feature_maps import merge_feature_maps
 from functions.misc.build_mesh import build_mesh, generalized_build_mesh
 from functions.orthogonal.bases import *
+from functions.orthogonal.find_optimal_design import find_optimal_design
 
-numel = 3
+numel = 20
 x = np.linspace(-1,1,numel)
 y = np.linspace(1.5,2,numel)
 z = np.linspace(-2,-1.5,numel)
 points_list = generalized_build_mesh([x,y,z])
 
-approx_degree = 4
+approx_degree = 5
 
 feature_map = legendre_features
 
@@ -20,4 +21,4 @@ for i in range(3):
 full_feature_map = merge_feature_maps(feature_maps)
 full_feature_map /= (full_feature_map.shape[1]**0.5)
 
-print(full_feature_map.shape)
+print(np.sum(find_optimal_design(full_feature_map)>0))
